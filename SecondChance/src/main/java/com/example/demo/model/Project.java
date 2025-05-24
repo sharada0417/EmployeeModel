dd9f1597cfd22ec0ff42160c4fd650f2cab466d3
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 16:10:17 +0530

    create relationship bet6ween project and employee

--- SecondChance/src/main/java/com/example/demo/model/Project.java
+++ SecondChance/src/main/java/com/example/demo/model/Project.java
@@ -1,7 +1,10 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Project {
@@ -9,4 +12,7 @@ public class Project {
 	private int project_id;
 	private String name;
 	private long totalcost;
+	
+	@ManyToMany(mappedBy="projects")
+	private List <Employee> employees;
 }
