18766b5e7b27c230eff36160dddecc87b666c337
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 16:22:07 +0530

    Create Department model relationship start with one to many and column

--- SecondChance/src/main/java/com/example/demo/model/Department.java
+++ SecondChance/src/main/java/com/example/demo/model/Department.java
@@ -1,5 +1,23 @@
 package com.example.demo.model;
 
-public class Department {
+import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Department {
+	@Id
+	@Column(name="dept_id")
+	private int id;
+	
+	@Column(nullable = false)
+	private String name;
+	private Date established;
+	
+	@OneToMany(mappedBy="department")
+	private List<Employee> employees;
 }
